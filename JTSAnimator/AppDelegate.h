2a155ffab53dcf6f85b566c839dba4499258b96b
Joshua Sullivan <jsulliva@nerdery.com>
2015-02-26 22:22:50 -0600

    Initial Commit

---
+++ JTSAnimator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTSAnimator
+//
+//  Created by Joshua Sullivan on 2/26/15.
+//  Copyright (c) 2015 Joshua Sullivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JTSAnimator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JTSAnimator
+//
+//  Created by Joshua Sullivan on 2/26/15.
+//  Copyright (c) 2015 Joshua Sullivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
