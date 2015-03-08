a2d2da9766a083b4f019596031011d3f78b77adf
Joshua Sullivan <jsulliva@nerdery.com>
2015-03-07 22:33:28 -0600

    Updating example project to test out the tweener.

--- JTSAnimator/ViewController.m
+++ JTSAnimator/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "JTSTweener.h"
+#import "JTSEaseQuadratic.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -19,9 +23,29 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+
+    }
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startTapped:(id)sender
+{
+    [JTSTweener tweenerWithDuration:4.0
+                      startingValue:0.0f
+                        endingValue:100.0f
+                        easingCurve:[JTSEaseQuadratic easeInOut]
+                            options:nil
+                      progressBlock:^(JTSTweener *tween, CGFloat value, NSTimeInterval elapsedTime) {
+                          self.label.text = [NSString stringWithFormat:@"%i", (int)value];
+                      }
+                    completionBlock:nil];
+
 }
 
 @end
