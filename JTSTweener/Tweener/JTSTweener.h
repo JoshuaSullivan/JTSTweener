2055471b23735f0725142e8eee90e8ddd4d946e2
Joshua Sullivan <jsulliva@nerdery.com>
2015-03-08 21:56:19 -0500

    Hurray comments!

--- JTSTweener/Tweener/JTSTweener.h
+++ JTSTweener/Tweener/JTSTweener.h
@@ -29,14 +29,42 @@ typedef void (^JTSTweenCompletionBlock)(JTSTweener *tween, BOOL completedSuccess
 
 @interface JTSTweener : NSObject
 
+/** The value that the tween will start at. The first progress invocation will be this value. */
 @property (assign, nonatomic) CGFloat startingValue;
+
+/** The value that the tween will end at. The last progress invocation will be this value.*/
 @property (assign, nonatomic) CGFloat endingValue;
+
+/** The duration of the tween, measured in seconds. */
 @property (assign, nonatomic) NSTimeInterval duration;
+
+/** The EasingCurve function being used in the tween. */
 @property (readonly, nonatomic) EasingCurve easingCurve;
+
+/** The options dictionary passed in at creation. This is not actually supported yet. */
 @property (readonly, nonatomic) NSDictionary *options;
+
+/** The play/paused state of the tween. */
 @property (readonly, nonatomic) BOOL isPaused;
+
+/** The completion state of the tween. */
 @property (readonly, nonatomic) BOOL isComplete;
 
+/**
+ *  Creates and returns a configured JTSTweener object. You do not need to retain the Tweener unless you want
+ *  to be able to pause/cancel it outside of the progress block. The tweener will be automatically destroyed
+ *  when it is completes.
+ *
+ *  @param duration        The duration of the tween in seconds.
+ *  @param startingValue   The value the tween starts on.
+ *  @param endingValue     The value the tween ends on.
+ *  @param easingCurve     The easing curve for the tween.
+ *  @param optionsOrNil    The options dictionary for the tween. Currently, no configuration options are supported, so this can be left as nil.
+ *  @param progressBlock   The callback block that is invoked every time the tweener changes its value.
+ *  @param completionBlock An optional callback block invoked after the final progress update.
+ *
+ *  @return Returns a configured and active JTSTweener object.
+ */
 + (JTSTweener *)tweenerWithDuration:(NSTimeInterval)duration
                       startingValue:(CGFloat)startingValue
                         endingValue:(CGFloat)endingValue
@@ -45,8 +73,13 @@ typedef void (^JTSTweenCompletionBlock)(JTSTweener *tween, BOOL completedSuccess
                       progressBlock:(JTSTweenProgressBlock)progressBlock
                     completionBlock:(JTSTweenCompletionBlock)completionBlock;
 
+/** Pause the current tween. Paused tweens do not invoke their progress block. */
 - (void)pause;
+
+/** Unpause the tween. The progress will resume where it left off when paused. */
 - (void)resume;
+
+/** Cancel a tween and mark it for destruction. NOTE: This will result in the completion block being invoked with a negative success. */
 - (void)cancel;
 
 
--- JTSTweener/Tweener/JTSTweener.m
+++ JTSTweener/Tweener/JTSTweener.m
@@ -43,16 +43,25 @@ static CFTimeInterval _prevTimestamp = -1.0;
     [self registerTweener:tweener];
     return tweener;
 }
+
 /**
-* This is the designated initializer for the class.
-*/
+ *  Discourage the manual instantiation of the class.
+ *
+ *  @return Always returns nil.
+ */
+- (instancetype)init
+{
+    NSAssert(NO, @"ERROR: The JTSTweener class should only be instantiated via the static 'tweenerWtihDuration:' method.");
+    return nil;
+}
+
 - (instancetype)initWithDuration:(NSTimeInterval)duration
                    startingValue:(CGFloat)startingValue
                      endingValue:(CGFloat)endingValue
                      easingCurve:(EasingCurve)easingCurve
                          options:(NSDictionary *)optionsOrNil
                    progressBlock:(JTSTweenProgressBlock)progressBlock
-                 completionBlock:(JTSTweenCompletionBlock)completionBlock
+                 completionBlock:(JTSTweenCompletionBlock)completionBlock 
 {
     self = [super init];
     if (!self) {
@@ -68,7 +77,6 @@ static CFTimeInterval _prevTimestamp = -1.0;
     self.progressBlock = progressBlock;
     self.completionBlock = completionBlock;
     _elapsedTime = 0.0;
-    [self update:0.0];
     return self;
 }
 
@@ -104,62 +112,129 @@ static CFTimeInterval _prevTimestamp = -1.0;
 
 - (void)destroy
 {
+    // Remove references to the easing curve and progress and completion blocks.
     _easingCurve = NULL;
     _progressBlock = NULL;
     _completionBlock = NULL;
 }
 
+/**
+ *  This method is invoked on every tick of the display link while the tweeener is not paused.
+ *
+ *  @param interval The time span (in seconds) since the previous update.
+ */
 - (void)update:(CFTimeInterval)interval
 {
+    // Keep track of the elapsed time.
     self.elapsedTime += interval;
+    
+    // If the elapsed time is greater than or equal to the duration, then the tween is complete.
     if (self.elapsedTime >= self.duration) {
-        self.elapsedTime = self.duration;
+        // Mark the tweener complete.
         self.isComplete = YES;
+        
+        // Final invocation of the progress block with the ending value.
+        if (self.progressBlock) {
+            self.progressBlock(self, self.endingValue, self.duration);
+        }
+        
+        // If there is a completion block, invoke it with success YES.
+        if (self.completionBlock) {
+            self.completionBlock(self, YES);
+        }
+        
+        // No need to make any of the calculations for this tweener.
+        return;
     }
-    CGFloat ratio = (CGFloat)fmin((self.elapsedTime / self.duration), 1.0);
+    
+    // Create the input ratio scalar value (percent of duration completed).
+    CGFloat ratio = (CGFloat)(self.elapsedTime / self.duration);
+    
+    // Run the ratio through the easing function to create the eased ratio.
     CGFloat easedRatio = self.easingCurve(ratio);
+    
+    // Use the eased ratio to compute the current tween value.
     CGFloat value = easedRatio * self.valueDifference + self.startingValue;
+    
+    // If there is a progress block (why wouldn't there be!?) invoke it with the updated value.
     if (self.progressBlock) {
         self.progressBlock(self, value, self.elapsedTime);
     }
-    if (self.isComplete && self.completionBlock) {
-        self.completionBlock(self, YES);
-    }
 }
 
+/**
+ *  Adds a tweener to the update list and ensures the Display Link is active.
+ *
+ *  @param tweener The JTSTweener instance to be added to the udpate list.
+ */
 + (void)registerTweener:(JTSTweener *)tweener
 {
+    // Create the update list if it doesn't already exist.
     if (!_tweeners) {
         _tweeners = [NSMutableArray array];
     }
+    
+    // Add the tweener to the list.
     [_tweeners addObject:tweener];
+    
+    // Invoke the tweener's update method with time zero so that its progress block is triggered with the exact starting value.
+    [tweener update:0.0];
+    
+    // If we haven't created the display link yet, do so now.
     if (!_displayLink) {
         _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(handleDisplayLinkTick:)];
         [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
     }
+    
+    // Make sure the display link isn't paused.
     _displayLink.paused = NO;
+    
+    // If the previous timestamp was the placeholder negative value, store the current media time so that we don't have
+    // a 1-frame lag before the animation starts.
+    if (_prevTimestamp < 0) {
+        _prevTimestamp = CACurrentMediaTime();
+    }
 }
 
+/**
+ *  When tweeners are active, this method tracks elapsed time-per-frame and invokes
+ *  the individual tweeners' update: method.
+ *
+ *  @param displayLink The CADisplayLink sending the update.
+ */
 + (void)handleDisplayLinkTick:(CADisplayLink *)displayLink
 {
+    // Calculate the elapsed time since the last invocation of this method.
     CFTimeInterval dt = displayLink.timestamp - _prevTimestamp;
-    if (_prevTimestamp < 0.0 || dt > kDroppedFrameThreshold) {
+    
+    // If it's been longer than the threshold time, drop the frame or the animation could go crazy.
+    if (dt > kDroppedFrameThreshold) {
         _prevTimestamp = displayLink.timestamp;
         return;
     }
+    
+    // Keep track of tweeners that have completed for removal from the list at the end of the loop.
     NSMutableArray *completedTweeners = [NSMutableArray array];
     for (JTSTweener *tweener in _tweeners) {
         if (tweener.isComplete) {
+            // The tweener is complete. Delete and remove it from the update list.
             [completedTweeners addObject:tweener];
             [tweener destroy];
         } else if (!tweener.isPaused) {
+            // If the tweener isn't paused, invoke its update: method.
             [tweener update:dt];
         }
     }
+    
+    // If we have any tweeners to remove, do so.
     if (completedTweeners.count > 0) {
         [_tweeners removeObjectsInArray:completedTweeners];
     }
+    
+    // Store the current timestamp for the next loop.
     _prevTimestamp = displayLink.timestamp;
+    
+    // If there are no more tweeners, pause the DisplayLink to save CPU power.
     if (_tweeners.count == 0) {
         _prevTimestamp = -1.0;
         _displayLink.paused = YES;
